e9ac8a01dc6ae6935d7eb39123a3d14cb3abaac0
salimDevv <mridhosalim18@gmail.com>
2022-03-28 16:04:43 +0700

    me

---
+++ app/src/main/java/com/example/ido/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.ido;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseUser firebaseUser;
+    private TextView textName;
+    private Button btnlogout;
+
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textName = findViewById(R.id.name);
+        btnlogout = findViewById(R.id.btn_logout);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser!=null){
+            textName.setText(firebaseUser.getDisplayName());
+        }else{
+            textName.setText("Login gagal");
+        }
+
+        btnlogout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            finish();
+        });
+    }
+}
